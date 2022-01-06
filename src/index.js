7ab1c9c71f16595318b72a860cc9482ac511013d
oiFernando23 <fernandozardopeixoto@hotmail.com>
2022-01-06 15:15:56 -0300

    .

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ const routes = require('./routes')
 const bodyParser = require('body-parser');
 const cors = require ('cors');
 
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
+// app.use((req, res, next) => {
+// 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+//     res.header("Access-Control-Allow-Origin", "*");
+// 	//Quais são os métodos que a conexão pode realizar na API
+//     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+//     app.use(cors());
+//     next();
+// });
+app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(routes);
