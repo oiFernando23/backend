0ddaefcb942dbe2ea922e5367e100d88f8dc371c
oiFernando23 <fernandozardopeixoto@hotmail.com>
2022-01-06 15:07:19 -0300

    .

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ const cors = require ('cors');
 
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    // res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     app.use(cors());
     next();
 });
