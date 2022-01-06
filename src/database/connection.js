99c31d3c7fec481d69787c93a269e3fc3e3e54e4
oiFernando23 <fernandozardopeixoto@hotmail.com>
2022-01-06 14:27:01 -0300

    tentando mais uma vez

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 //database', 'username', 'password'
-const sequelize = new Sequelize('d93gdakedvror9', 'postgres', '6c9c0467498c768e58a8a9297912e4d6c1e9e7733d1e26fa1d1e71d4be4a01ba', {
+const sequelize = new Sequelize('d93gdakedvror9', 'd93gdakedvror9', '6c9c0467498c768e58a8a9297912e4d6c1e9e7733d1e26fa1d1e71d4be4a01ba', {
     host: 'ec2-54-83-152-251.compute-1.amazonaws.com',
     dialect: 'postgres'
 });
@@ -17,9 +17,9 @@ db.posts = require('./models/Posts')(sequelize, Sequelize);
 db.users.hasMany(db.posts);
 db.posts.belongsTo(db.users);
 
-// sequelize.sync({
-//     alter: true
-// })
+sequelize.sync({
+    alter: true
+})
 
 db.sequelize.authenticate().then(() => {
     console.log("Banco conectado com sucesso :)");
